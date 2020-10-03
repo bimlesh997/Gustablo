3b4080e5e525badd0a70dd1e0e1888212868a103
bimlesh997 <bimlesh15.info@gmail.com>
2020-10-03 18:40:02 +0530

    my first commit

---
+++ js/main.js
@@ -0,0 +1,67 @@
+$('#slider-area').owlCarousel({
+    loop: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 1
+        },
+        1000: {
+            items: 1
+        }
+    }
+})
+//testimonail slider
+$('#testimonials-slider').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    navText: ["<div class='nav-btn prev-slide'></div>", "<div class='nav-btn next-slide'></div>"],
+    responsive: {
+        0: {
+            items: 1
+        },
+        768: {
+            items: 2
+        },
+        1000: {
+            items: 3
+        }
+    }
+})
+
+// header fixed
+$(function () {
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+});
+//counter
+$('.counter-value').each(function () {
+    var $this = $(this),
+        countTo = $this.attr('data-count');
+    $({ countNum: $this.text() }).animate({
+        countNum: countTo
+    },
+        {
+            duration: 8000,
+            easing: 'linear',
+            step: function () {
+                $this.text(Math.floor(this.countNum));
+            },
+            complete: function () {
+                $this.text(this.countNum);
+                //alert('finished');
+            }
+        });
+});
+
+
+
+
+
